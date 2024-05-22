a4146970c30f69485c5ac7d1112ad6859b34746d
Jacob <jacobkellogg17@gmail.com>
2024-05-22 14:12:33 -0500

    Fixed Application Context and getbean

--- src/main/java/jacobland/org/mailsystem/MailSystemApplication.java
+++ src/main/java/jacobland/org/mailsystem/MailSystemApplication.java
@@ -1,22 +1,24 @@
 package jacobland.org.mailsystem;
 
 import jacobland.org.mailsystem.model.Message;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MailSystemApplication {
 
 	public static void main(String[] args) {
 
-        ApplicationContext context = (ApplicationContext) SpringApplication.run(MailSystemApplication.class, args);
+        ApplicationContext context = SpringApplication.run(MailSystemApplication.class, args);
 
-        Message message1 = context.get
+        Message message1 = context.getBean(Message.class);
 		message1.setId(12);
 		message1.setTitle("Hello");
 		message1.setMessage(("Hello, how are you today?"));
+
+
 
 
 	}
--- src/main/java/jacobland/org/mailsystem/model/data/MessageRepo.java
+++ src/main/java/jacobland/org/mailsystem/model/data/MessageRepo.java
@@ -1,4 +1,19 @@
 package jacobland.org.mailsystem.model.data;
 
+import jacobland.org.mailsystem.model.Message;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class MessageRepo {
+
+    public void save(Message message){
+        System.out.println("Added");
+    }
+
+    public List<Message> findAll() {
+        return new ArrayList<Message>();
+    }
 }
