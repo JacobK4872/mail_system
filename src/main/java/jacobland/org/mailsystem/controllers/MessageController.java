7bb9d1e694c1fa195753b9787d590afccb8d3583
Jacob <jacobkellogg17@gmail.com>
2024-06-05 17:56:04 -0500

    Create message method

--- src/main/java/jacobland/org/mailsystem/controllers/MessageController.java
+++ src/main/java/jacobland/org/mailsystem/controllers/MessageController.java
@@ -5,6 +5,7 @@ import jacobland.org.mailsystem.model.data.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +22,19 @@ public class MessageController {
         return messageRepo.findAll();
     }
 
-    @GetMapping("goodbye")
+    @PostMapping("createMessage")
     @ResponseBody
-    public String goodbye() {
-        return "Goodbye, Spring!";
+    public String createMessage() {
+
+        Message message = new Message();
+
+//        message.setId(27);
+//		message.setTitle("Your mom");
+//		message.setMessage(("Hows your mom ?"));
+
+        messageRepo.save(message);
+
+        return "saved";
     }
 
 //    int result = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM MAIL_SYSTEM", integer.class);
