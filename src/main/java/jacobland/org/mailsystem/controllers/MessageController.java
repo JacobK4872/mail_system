0738b06064b09657325436465db9fdf4fbce4225
Jacob <jacobkellogg17@gmail.com>
2024-06-05 13:46:59 -0500

    now all data shows up when makeing a request at /message

--- src/main/java/jacobland/org/mailsystem/controllers/MessageController.java
+++ src/main/java/jacobland/org/mailsystem/controllers/MessageController.java
@@ -1,16 +1,24 @@
 package jacobland.org.mailsystem.controllers;
 
+import jacobland.org.mailsystem.model.Message;
+import jacobland.org.mailsystem.model.data.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MessageController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
 
     @GetMapping("message")
     @ResponseBody
-    public String message() {
-        return "Hello, Spring!";
+    public Iterable<Message> getAllMessages() {
+        return messageRepo.findAll();
     }
 
     @GetMapping("goodbye")
--- src/main/java/jacobland/org/mailsystem/model/data/MessageRepo.java
+++ src/main/java/jacobland/org/mailsystem/model/data/MessageRepo.java
@@ -3,8 +3,11 @@ package jacobland.org.mailsystem.model.data;
 import jacobland.org.mailsystem.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +34,24 @@ public class MessageRepo {
     }
 
     public List<Message> findAll() {
-        return new ArrayList<Message>();
+
+        String sql = "select * from messages";
+
+        RowMapper<Message> mapper = new RowMapper<Message>() {
+            @Override
+            public Message mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Message a = new Message();
+                a.setId(rs.getInt(1));
+                a.setTitle(rs.getString(2));
+                a.setMessage(rs.getString(3));
+
+                return a;
+            }
+        };
+
+        List<Message> messages = template.query(sql, mapper);
+
+        return messages;
     }
 }
